11c95fad2e29223b9d21f77aff33eda08a8e2550
Augusto Godoy <augustog@Augustos-MacBook-Air.local>
2021-09-18 20:33:04 -0300

    Dockerize Project

---
+++ API/README.md
@@ -0,0 +1,7 @@
+# PE_Api
+Personal Economy Api project - NodeJS
+
+# How to execute :
+sudo docker-compose build
+
+sudo docker-compose up
---
+++ API/app.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-console */
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+
+const options = {
+  inflate: true,
+  limit: '100kb',
+};
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.raw(options));
+
+const auth = require('./app/entities/auth/route/auth.route');
+app.use(`/api/auth`, auth);
+var port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`/api/auth`);
+});
